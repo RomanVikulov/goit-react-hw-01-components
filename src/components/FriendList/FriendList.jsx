7154aff78adb6cae07e3ba1a941f2515fdf44430
Roman <romanuch06.88@gmail.com>
2022-10-22 16:02:22 +0300

    add friend

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import { FriendlistItem } from './FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendlistItem
+          key={id}
+          id={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};

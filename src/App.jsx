de571c5321de591658436ef1a4cb2a9c8eac12b2
Roman <romanuch06.88@gmail.com>
2022-10-22 18:27:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import css from 'App.module.css';
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+import { Profile } from './components/Profile/Profile';
+import { Statistics } from './components/Statistics/Statistics';
+import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from 'components/Transaction/TransactionHistory';
+
 export const App = () => {
   return (
     <div className={css.App}>
